8a67bc051937395754ea07478ef0be2e1816cdea
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2026-03-17 14:24:22 -0600

    Delete osx directory

--- osx/makebundle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -rf ZoiteChat.app
-rm -f *.app.zip
-
-python $HOME/.local/bin/gtk-mac-bundler zoitechat.bundle
-
-echo "Compressing bundle"
-zip -9rXq ./ZoiteChat-$(git describe --tags).app.zip ./ZoiteChat.app
